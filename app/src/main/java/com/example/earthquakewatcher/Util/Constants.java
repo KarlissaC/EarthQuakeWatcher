0709e8790151166b66c37b2c18ebe8db773bb83d
KarlissaC <karlissacole2@gmail.com>
2020-02-26 04:07:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/earthquakewatcher/Util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.earthquakewatcher.Util;
+
+public class Constants {
+   public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+   public static final int LIMIT = 30;
+
+
+}
